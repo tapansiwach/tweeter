572896d8a29a19d1ed98b15225cb1fb89c0d9794
Tapan Siwach <siwach.tapan@gmail.com>
2021-09-07 03:57:52 -0400

    adjust the code so the counter turns red when invalid

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
 
   $(".new-tweet form textarea").on("input", function(event) {
-    const remainingChars = 140 - this.value.length;
+    const remainingChars = 10 - this.value.length;
     const output = $(this).next().children()[1];
     output.textContent = remainingChars;
+    output.style.color = remainingChars > 0 ? "black" : "red";
   });
 
 });
