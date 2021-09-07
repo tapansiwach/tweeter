7ed9716206b96205f9f2f65f1c4540fbb118ad9b
Tapan Siwach <siwach.tapan@gmail.com>
2021-09-07 04:45:21 -0400

    fix code: maximum chars allowed is 140

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
   $(".new-tweet form textarea").on("input", function(event) {
-    const remainingChars = 10 - this.value.length;
+    const remainingChars = 140 - this.value.length;
     const output = $(this).next().children()[1];
     output.textContent = remainingChars;
     output.style.color = remainingChars > 0 ? "black" : "red";
