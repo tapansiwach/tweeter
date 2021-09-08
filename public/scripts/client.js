480d9d0670c5e8baef9603719924ae71941f482f
Tapan Siwach <siwach.tapan@gmail.com>
2021-09-08 15:28:35 -0400

    create a function that is responsible for fetching tweets from the server

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,32 +4,32 @@
 * Reminder: Use (and do all your DOM work in) jQuery's document ready function
 */
 
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+// // Fake data taken from initial-tweets.json
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd"
+//     },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ]
 
 
 const createTweetElement = tweetObj => {
@@ -61,7 +61,7 @@ const createTweetElement = tweetObj => {
 
 const renderTweets = function(tweets) {
   // loops through tweets
-  for (const item of data) {
+  for (const item of tweets) {
     // calls createTweetElement for each tweet
     const $tweet = createTweetElement(item);
     // takes return value and appends it to the tweets container
@@ -70,7 +70,7 @@ const renderTweets = function(tweets) {
 }
 
 
-renderTweets(data);
+// renderTweets(data);
 
 
 // add an event listener for form submissions
@@ -86,3 +86,14 @@ $('.new-tweet form').on('submit', function(event) {
   });
   this.reset(); // use native javascript to reset values in form
 });
+
+
+// get tweets from server and render them to screen
+
+const loadTweets = () => {
+  $.get('/tweets', function(response) {
+    renderTweets(response);
+  });
+}
+
+loadTweets();
